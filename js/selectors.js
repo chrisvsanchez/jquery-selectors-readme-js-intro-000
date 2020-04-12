7db9691a84700281779b498c8fdb7431e624a457
Chris Sanchez <chrisvsanchez@gmail.com>
2020-04-12 00:24:53 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+
+}
+
+function ninjaBabySelector(){
+  return $('')
+}
+
+function divSelector(){
+  
+}
+
+function firstListItem(){
+  
+}
